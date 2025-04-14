c36d48eac623cc53d8dcf9c9519b29ff931268b5
Caleb <jcaleb251@gmail.com>
2025-04-14 01:22:53 -0400

    Still can't install dependencies

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 /// Imports
 const express = require("express");
 const path = require('path');
-const getId = require('./utils/getId');
 
 
+const {
+    serveCars,
+    serveCar,
+    createCar,
+    updateCar,
+    deleteCar
+  } = require('./controllers/CarControllers');
+  
 
 const app = express();
 const pathToFrontendDist = path.join(__dirname, '../frontend/dist');
@@ -21,6 +28,19 @@ const logRoutes = (req, res, next) => {
 
   const serveStatic = express.static(pathToFrontendDist);
 
+  const parseJSON = express.json();
+  
+  app.use(logRoutes);   
+  app.use(serveStatic);
+  app.use(parseJSON);   
+  
+
+  app.get('/api/fellows', serveFellows);
+  app.get('/api/fellows/:id', serveFellow);
+  app.post('/api/fellows', createFellow);
+  app.patch('/api/fellows/:id', updateFellow);
+  app.delete('/api/fellows/:id', deleteFellow);
+  
 
   const port = 8080;
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
